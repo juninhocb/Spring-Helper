bea2761b35efe0f3a18c01402513fa87cf8fac04
juninhocb <juninhocb@hotmail.com>
2023-06-10 08:57:32 -0300

    first_commit

---
+++ microclient/src/main/java/com/carlosjr/microclient/MicroclientApplication.java
@@ -0,0 +1,13 @@
+package com.carlosjr.microclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroclientApplication.class, args);
+	}
+
+}
---
+++ microclient/src/test/java/com/carlosjr/microclient/MicroclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosjr.microclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
