845dea99c06db809253ab3ac06a32d43af1c6523
juninhocb <juninhocb@hotmail.com>
2023-06-26 11:25:18 -0300

    embedded server

---
+++ jms-service/src/main/java/com/carlosjr/jmsservice/listener/HelloMessageListener.java
@@ -0,0 +1,42 @@
+package com.carlosjr.jmsservice.listener;
+
+import com.carlosjr.jmsservice.config.JmsConfig;
+import com.carlosjr.jmsservice.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class HelloMessageListener {
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message){
+
+        //System.out.println("I got a message!!!");
+        //System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RECEIVE_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) throws JMSException {
+        HelloWorldMessage messageWorld = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), messageWorld);
+
+    }
+}
---
+++ jms-service/src/test/java/com/carlosjr/jmsservice/JmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosjr.jmsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
