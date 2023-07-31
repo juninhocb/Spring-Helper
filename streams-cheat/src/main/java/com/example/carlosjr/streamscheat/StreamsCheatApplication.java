40e5def96e658836953677111446186a91de9344
juninhocb <juninhocb@hotmail.com>
2023-07-31 17:19:22 -0300

    streams

---
+++ streams-cheat/src/main/java/com/example/carlosjr/streamscheat/StreamsCheatApplication.java
@@ -0,0 +1,13 @@
+package com.example.carlosjr.streamscheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsCheatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamsCheatApplication.class, args);
+    }
+
+}
