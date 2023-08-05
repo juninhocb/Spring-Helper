64d4352844fec25640b9eedbb507def40d38ccd2
juninhocb <juninhocb@homail.com>
2023-08-05 00:48:36 -0300

    spring-web-mvc

---
+++ spring-web-mvc/src/main/java/com/example/carlosjr/springwebmvc/resources/MyResource.java
@@ -0,0 +1,19 @@
+package com.example.carlosjr.springwebmvc.resources;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MyResource {
+
+    @GetMapping("/stub")
+    public ResponseEntity<String> restfullEndpoint(){
+        return new ResponseEntity<>("Welcome!", HttpStatus.OK);
+    }
+
+
+}
