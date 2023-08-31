e04743e85a635e00a13608dc78d3c00c63576932
juninhocb <juninhocb@hotmail.com>
2023-08-31 14:47:21 -0300

    fix step job problem

--- batch-process/src/test/java/com/example/carlosjr/batchprocess/BatchProcessApplicationTests.java
+++ batch-process/src/test/java/com/example/carlosjr/batchprocess/BatchProcessApplicationTests.java
@@ -13,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBootTest
 @ExtendWith(OutputCaptureExtension.class)
 class BatchProcessApplicationTests {
@@ -35,6 +38,21 @@ class BatchProcessApplicationTests {
         // then
         Assertions.assertTrue(output.getOut().contains("Processing the billing information!"));
         Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+
+    @Test
+    void testJobExecution() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("input.file", "src/main/resources/billing-2023-01.csv")
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = this.jobLauncher.run(job, jobParameters);
+
+        // then
+        Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+        Assertions.assertTrue(Files.exists(Paths.get("staging", "billing-2023-01.csv")));
     }
 
 }
